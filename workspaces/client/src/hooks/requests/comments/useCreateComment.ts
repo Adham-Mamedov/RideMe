1522a7993cc360d6e65f9f41ebd66dc6b9d29013
Adham-Mamedov <adhammamedov@mail.ru>
2023-04-06 11:20:56 +0500

    fix(client): fix redirect to login page after logout from home page

--- workspaces/client/src/hooks/requests/comments/useCreateComment.ts
+++ workspaces/client/src/hooks/requests/comments/useCreateComment.ts
@@ -2,6 +2,7 @@ import { useMutation } from 'react-query';
 
 import { useAuthAxios } from '@client/hooks/useAuthAxios';
 import { useNotificationStore } from '@client/stores/NotificationStore';
+import { useAuthStore } from '@client/stores/AuthStore';
 
 import { CreateCommentDto } from '@server/modules/comment/dto/comment.dto';
 import { EReactQueryKeys, ERoute } from '@shared/enums';
@@ -9,8 +10,9 @@ import { IComment } from '@shared/types/assets.types';
 
 const useCreateComment = () => {
   const displaySuccess = useNotificationStore((state) => state.displaySuccess);
+  const user = useAuthStore((state) => state.user);
 
-  const axios = useAuthAxios();
+  const axios = useAuthAxios(!user);
 
   return useMutation(
     EReactQueryKeys.commentsCreate,
